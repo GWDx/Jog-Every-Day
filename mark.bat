e4b778a298eacaef1f140c80f6475918b6e11c14
GWDx <gwdx@mail.ustc.edu.cn>
2021-03-07 22:15:50 +0800

    2021-03-07

--- mark.bat
+++ mark.bat
@@ -1,7 +1,7 @@
-cd..
 git pull
 set da=%date:/=-%
-echo %da:~0,10% : 1 >> Jog-Every-Day\record\%da:~0,7%.txt
+echo %da:~0,10% : 1 >> record\%da:~0,7%.txt
 git add record
 git commit -m "%da:~0,10%"
 git push
+pause
