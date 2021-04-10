1ff4825572a170ccd9d106e1ed7fc3347c32faa1
GWDx <gwdx@mail.ustc.edu.cn>
2021-04-10 17:06:48 +0800

    modify bat

--- mark.bat
+++
@@ -1,7 +0,0 @@
-git pull
-set da=%date:/=-%
-echo %da:~0,10% : 1 >> record\%da:~0,7%.txt
-git add record
-git commit -m "%da:~0,10%"
-git push
-pause
--- mark2.bat
+++ mark2.bat
@@ -1,13 +1,16 @@
+set valid=%date:~-10%
 set da=%date:/=-%
 set write=%da:~0,10% : 1
 set file=record\%da:~0,7%.txt
+
+echo %write%
+pause
 
 for /f %%i in (%file%) do set lastline=%%i
 
 if "%lastline%" neq "%write%" (
 	echo %write% >> %file%
-	git add record
+	git add %file%
 	git commit -m "%da:~0,10%"
-	git push
 )
 pause
